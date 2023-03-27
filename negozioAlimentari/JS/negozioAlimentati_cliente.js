32e777776f3416929882b6ea74cf0dd1025b8bee
Elias Ceccarini <126388339+EliasCeccarini@users.noreply.github.com>
2023-03-27 16:54:57 +0200

    Create negozioAlimentati_cliente.js

---
+++ negozioAlimentari/JS/negozioAlimentati_cliente.js
@@ -0,0 +1,37 @@
+class Cliente{
+     #m_nome;
+     #m_cognome;
+     #m_codice;
+     #m_saldo;
+     constructor(nome, cognome, codice, saldo){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.codice = codice;
+        this.saldo = saldo;
+    }
+    get nome(){
+    return this.#m_nome;
+    }
+    get cognome(){
+    return this.#m_cognome;
+    }
+    get codice(){
+    return this.#m_codice;
+    }
+    get saldo(){
+    return this.#m_saldo;
+    }      
+    set nome(nuovoNome){         
+    this.#m_nome = nuovoNome;
+    }      
+    set cognome(nuovoCognome){         
+    this.#m_cognome = nuovoCognome;
+    }
+	  set codice(nuovoCodice){
+	  this.#m_codice = nuovoCodice;
+	  }
+	  set saldo(nuovoSaldo){
+	  Math.floor(Math.random() * 101);
+	  }
+}     
+	let cliente = new Cliente("ofosfe", "oiswfwef", 987456544, 1);     console.log(cliente);
