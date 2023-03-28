b6effdd6a969f179e6cc8c4ae13d737c416090a4
Thomas Merlini <thomas.merlini@studenti.isissgobetti.it>
2023-03-28 17:17:56 +0200

    sistemato JS negozio

--- negozioAlimentari/JS/negozioAlimentari_negozio.js
+++ negozioAlimentari/JS/negozioAlimentari_negozio.js
@@ -8,6 +8,14 @@ class Negozio{
         this.#m_prodotti_disponibili = prodottiDisponibili;
     }
 
+    get nome(){
+        return this.#m_nome;
+    }
+
+    get indirizzo(){
+        return this.#m_indirizzo;
+    }
+
     get prodottiDisponibili(){
         return this.#m_prodotti_disponibili;
     }
@@ -15,6 +23,21 @@ class Negozio{
     getNProdotti(){
         return this.prodottiDisponibili.length;
     }
+
+    getValProdotti(){
+        
+    }
 }
 
+const prodottiDisponibili = [];
+
+for (let i = 0; i < 15; i++) {
+    prodottiDisponibili[i] = "Prodotto "+(i+1);
+}
+
+const negozio = new Negozio("Ciccio","Via del Cavolo",prodottiDisponibili);
+document.writeln("Nome: " + negozio.nome + "<br>");
+document.writeln("Indirizzo: " + negozio.indirizzo + "<br>");
+document.writeln("Prodotti disponibili: " + negozio.prodottiDisponibili + "<br>");
+document.writeln("Lunghezza prodotti disponibili: " + negozio.getNProdotti() + "<br>");
 
